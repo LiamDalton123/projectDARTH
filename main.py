e662c690b83c0141e0ec93f2bddd6a09816bc62c
LiamDalton123 <liamdalton79@gmail.com>
2022-03-06 18:21:27 +0000

    06/03/22 GT array

--- main.py
+++ main.py
@@ -5,34 +5,83 @@ import numpy as np
 from scipy.io import wavfile
 from scipy import signal
 import matplotlib.pyplot as plt
-
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+from datetime import time
 from VAD import VAD
 from ConfigVAD import *
 
-# data = data / 32768
 
+# data = data / 32768
 def displayInfo():
     fileList = os.listdir("test_files/tt_off")
-    logging.info("File list: "+str(fileList))
+    logging.info("File list: " + str(fileList))
     for file in fileList:
-        if file.endswith(".wav"):
-            filepath = "test_files/tt_off/"+file
-            samplerate, data = wavfile.read(filepath)
-            logging.info("File name: "+file)
-            logging.info("Sample rate: " + str(samplerate))
-            logging.info("Length of Data: " + str(len(data)))
-            plt.figure()
-            plt.subplot(211)
-            plt.plot(data)
-            plt.xlabel = "Sample"
-            plt.ylabel = "Amplitude"
-            plt.subplot(212)
-            freqs, times, Sxx = signal.stft(data / 32768, fs=samplerate, window=np.hanning(2048), nfft=2048, nperseg=2048,
-                                            noverlap=1024, return_onesided=True)
-            plt.pcolor(times, freqs, np.abs(Sxx)) # Sxx as log
-            plt.show()
+        if file.endswith(".wav") and "_Int" in file:
+            filepath = "test_files/tt_off/" + file
+            displayFileInfo(filepath)
 
 
+def displayFileInfo(filepath):
+    samplerate, data = wavfile.read(filepath)
+    data = data[:30 * samplerate]
+    logging.info("File name: " + filepath)
+    logging.info("Sample rate: " + str(samplerate))
+    logging.info("Length of data: " + str(len(data)))
+    gt_array = loadGroundTruthArray(filepath + ".gt", 30)
+    plt.figure()
+    plt.subplot(311)
+    plt.plot(data[:30 * samplerate:1000])
+    plt.title = filepath
+    plt.xlabel("Sample")
+    plt.ylabel("Amplitude")
+
+    plt.subplot(312)
+    plt.plot(gt_array)
+    plt.xlabel("Time (s)")
+    plt.ylabel("Speech")
+
+    plt.subplot(313)
+    freqs, times, Sxx = signal.stft(data / 32768, fs=samplerate, window=np.hanning(2048), nfft=2048, nperseg=2048,
+                                    noverlap=1024, return_onesided=True)
+    plt.pcolor(times, freqs, np.log2(np.abs(Sxx)))  # Sxx as log
+
+    plt.show()
+
+
+def loadGroundTruthArray(gt_filename, duration_in_seconds):
+    gt_array = []
+    current_time_in_seconds = 0
+    with open(gt_filename) as file:
+        lines = file.readlines()
+    for line in lines:
+        start_time_string, end_time_string = line.strip().split("/")
+        logging.info("Start time: " + start_time_string)
+        logging.info("End time: " + end_time_string)
+        start_time = time.fromisoformat(start_time_string)
+        end_time = time.fromisoformat(end_time_string)
+        start_time_in_seconds = (start_time.hour * 60 + start_time.minute) * 60 + start_time.second
+        end_time_in_seconds = (end_time.hour * 60 + end_time.minute) * 60 + end_time.second
+        if end_time.microsecond != 0:
+            end_time_in_seconds += 1
+
+        # For time up until speech starts mark it as non-speech (0):
+        for second in range(current_time_in_seconds, start_time_in_seconds):
+            gt_array.append(0)
+
+        # For time between start and end of speech mark it as speech (1):
+        for second in range(start_time_in_seconds, end_time_in_seconds):
+            gt_array.append(1)
+
+        current_time_in_seconds = end_time_in_seconds
+        print("Ground truth array: " + str(gt_array))
+
+    if duration_in_seconds > end_time_in_seconds:
+        for second in range(end_time_in_seconds, duration_in_seconds):
+            gt_array.append(0)
+
+    print("Ground truth array: " + str(gt_array))
+    return gt_array
 
 def categorize():
     VAD.displayVADConfiguration()
@@ -45,7 +94,7 @@ def categorize():
     startRange = 0
     endRange = samplerate
     while endRange < len(data):
-        buffer = data[startRange:endRange]/32768
+        buffer = data[startRange:endRange] / 32768
         result = VAD.classifyFrame(buffer, window_size=ConfigVAD.NO_OF_SECONDS * ConfigVAD.FREQUENCY)
         result_array = np.append(result_array, result)
         logging.info("classifyFrame result = " + str(result))
@@ -57,7 +106,10 @@ def categorize():
 
 
 def main():
-    categorize()
+    Tk().withdraw()
+    filename = askopenfilename()
+    displayFileInfo(filename)
+    # categorize()
 
 
 if __name__ == "__main__":
